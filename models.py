937ecc522413f6b7970d4dc3cd69c17a5ed82994
Erick Oliveros <eoliveros@redratclothing.co.nz>
2021-04-16 03:01:28 +0000

    Attempting Moving away from User.wallet_address to Seeds.wallet_address

--- models.py
+++ models.py
@@ -363,7 +363,9 @@ class MerchantTx(db.Model):
 #            session.commit()
 
     def update_wallet_address(cls, session, user):
-        seeds = db.session.query(Seeds).filter(Seeds.user_id == user.id):
+        seeds = db.session.query(Seeds).filter(Seeds.user_id == user.id)
+        print(seeds)
+        logger.info(seeds)
         if seeds.wallet_address:
             # update txs
             limit = 100
